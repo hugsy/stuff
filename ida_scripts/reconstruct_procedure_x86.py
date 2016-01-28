5524ab2c94642715f12b9f457179f247bed47bf8
hugsy <hugsy+github@pyc.li>
2016-01-28 13:31:31 +1100

    the real thing

--- ida_scripts/reconstruct_procedure_x86.py
+++ ida_scripts/reconstruct_procedure_x86.py
@@ -1,6 +1,6 @@
 """
-This IDA simple script will look for function prolog sequence for x86 binaries
-i.e. (in intel syntax)
+This simple script for IDA will look for function prolog sequence for x86 binaries
+i.e. (in Intel syntax)
 
 push ebp
 mov ebp, esp
@@ -10,18 +10,36 @@ And will make IDA treat it as a procedure.
 @_hugsy_
 """
 
-from idc import *
-from idaapi import *
+import idc
+import idaapi
+import sys
 import idautils
 
-prolog_sequence = "\x55\x89\xe5"
+hilight_color = 0x009900
+prolog_sequence = "55 89 e5"
+ea = idc.ScreenEA()
 
-for seg in idautils.Segments():
-    print seg
-    if seg == ".text":
-        found = True
+addr = idc.SegStart(ea)
+print "[!] Analyzing from %#x" % addr
+
+while True:
+    res = idc.FindBinary(addr, idaapi.BIN_SEARCH_FORWARD, prolog_sequence, 16)
+    if res == idaapi.BADADDR:
         break
 
-if found:
-    addr = seg
-    # DecodeInstruction
+    print "[+] Matching at %#x" % res
+    idc.Jump(res)
+    col = idc.GetColor(res, idc.CIC_ITEM)
+    idc.SetColor(res, idc.CIC_ITEM, hilight_color)
+    idc.SetColor(res + 1, idc.CIC_ITEM, hilight_color)
+
+    ret = idc.AskYN(0, "Would you like to create a function at %#x ?" % res)
+    if ret == 1:
+        idc.MakeFunction(res)
+        print "[+] Creating function at %#x" % res
+
+    idc.SetColor(res, idc.CIC_ITEM, col)
+    idc.SetColor(res + 1, idc.CIC_ITEM, col)
+    addr = res + len(prolog_sequence)
+
+print "[!] EOT"
