4755d5795b40d402994aa5f04470c2ed3faccff1
crazy rabbidz <hugsy@users.noreply.github.com>
2019-09-05 11:04:09 -0700

    Update copy_rva.py

--- ida_scripts/copy_rva.py
+++ ida_scripts/copy_rva.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7 -tt
+#!/usr/bin/env python2.7
 # -*- mode: python -*-
 # -*- coding: utf-8 -*-
 
@@ -10,44 +10,66 @@ import os, Tkinter
 import idaapi, idc
 
 PLUGIN_NAME = "CopyRva"
-PLUGIN_HOTKEY = "Ctrl-Alt-G"
+PLUGIN_HOTKEY = "Ctrl-Alt-H"
 PLUGIN_VERSION = "0.1"
 PLUGIN_AUTHOR = "@_hugsy_"
 
 
-def copy_to_clipboard(addr):
-    name = os.path.splitext(idc.GetInputFile())[0]
-    f = "{:s}+{:x}".format(name, addr)
-    r = Tkinter.Tk()
-    r.withdraw()
-    r.clipboard_clear()
-    r.clipboard_append("{:s}".format(f))
-    r.update()
-    r.destroy()
-    print("[+] Copied {:s}".format(f))
-    return
+class CopyRvaPlugin(idaapi.plugin_t):
+
+    flags = idaapi.PLUGIN_UNL
+    comment = "Quickly copy to clipboard the position of the cursor in IDA Pro"
+    help = "Quickly copy to clipboard the position of the cursor in IDA Pro"
+    wanted_name = PLUGIN_NAME
+    wanted_hotkey = PLUGIN_HOTKEY
 
 
-def get_rva():
-    rva = here() - idaapi.get_imagebase()
-    return rva
+    def init(self):
+        self.enable_shortcut()
+        print("[+] Press '{:s}' to copy RVA to clipboard".format(PLUGIN_HOTKEY))
+        return
 
 
-def copy2clip():
-    copy_to_clipboard( get_rva() )
-    return
+    def copy_to_clipboard(self, addr):
+        name = os.path.splitext(idc.GetInputFile())[0]
+        f = "{:s}+{:x}".format(name, addr)
+        r = Tkinter.Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append("{:s}".format(f))
+        r.update()
+        r.destroy()
+        print("[+] Copied {:s}".format(f))
+        return
 
 
-def enable_shortcut():
-    idaapi.CompileLine('static copy2clip() { RunPythonStatement("copy2clip()"); }')
-    idc.AddHotkey(PLUGIN_HOTKEY, "copy2clip")
-    return
+    def get_rva(self):
+        ea = idc.ScreenEA()
+        base = idaapi.get_imagebase()
+        rva = ea - base
+        return rva
+
+
+    def copy2clip(self):
+        self.copy_to_clipboard( self.get_rva() )
+        return
+
+
+    def enable_shortcut(self):
+        idaapi.CompileLine('static copy2clip() { RunPythonStatement("copy2clip()"); }')
+        idc.AddHotkey(PLUGIN_HOTKEY, "copy2clip")
+        return
+
+
+    def run(self, _=0):
+        pass
+
+
+    def term(self):
+        pass
+
 
 
 def PLUGIN_ENTRY():
-    enable_shortcut()
-    print("[+] Press '{:s}' to copy RVA to clipboard".format(PLUGIN_HOTKEY))
-    return
+    return CopyRvaPlugin()
 
-if __name__ == "__main__":
-    PLUGIN_ENTRY()
