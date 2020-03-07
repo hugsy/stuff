43485f4a4a9c83b36dfa02699cb88ae3588fa0f2
crazy rabbidz <hugsy@users.noreply.github.com>
2020-03-06 16:56:29 -0800

    Create ida_default_structure.py

---
+++ ida_scripts/ida_default_structure.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+"""
+stupid script to generate IDA structure the way I like
+"""
+
+
+name = sys.argv[1]
+size = int(sys.argv[2])
+ptrsize = int(sys.argv[3]) if len(sys.argv) == 4 else 8
+struct_name = name.upper()
+print("""typedef struct _{} {{""".format(struct_name))
+for idx, offset in enumerate(range(0, size, ptrsize)):
+    print("    /* {:04x} */  ULONG_PTR Unknown{};".format(offset, idx))
+print("""}} {0}, *P{0};""".format(struct_name))
