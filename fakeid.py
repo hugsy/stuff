7e55052489ad52f39d0ab692a5433cf77f30eee7
hugsy <hugsy@blah.cat>
2020-05-31 08:59:37 -0700

    updated fakeid for py3

--- fakeid.py
+++ fakeid.py
@@ -3,14 +3,13 @@
 import argparse
 import sys
 import time
-import lxml
 
 import requests
 import bs4
 
 
 __author__    =   "hugsy"
-__version__   =   0.1
+__version__   =   0.2
 __licence__   =   "WTFPL v.2"
 __file__      =   "fakeid.py"
 __desc__      =   """fakeid.py: generates fake people profile"""
@@ -20,12 +19,16 @@ syntax: {0} [options] args
 """.format(__version__, __licence__, __author__)
 
 
-RANDOM_ID_URL = "http://www.fakenamegenerator.com/gen-random-us-us.php"
+RANDOM_ID_URL = "https://www.fakenamegenerator.com/gen-random-us-us.php"
+
 
 class RandomPerson:
 
     def __init__(self):
-        self.soup = bs4.BeautifulSoup(self.get_page_text(), "lxml")
+        text = self.get_page_text()
+        if text is None:
+            raise Exception
+        self.soup = bs4.BeautifulSoup(text, features="html.parser")
         d = self.soup.find("div", "address")
         self.firstname, self.lastname = d.h3.string.rsplit(" ", 1)
         self.address = d.find("div", "adr").text.strip()
@@ -43,9 +46,18 @@ class RandomPerson:
         return self.soup.find("dt", text=name).next_sibling.next_sibling
 
     def get_page_text(self):
-        h = requests.get(RANDOM_ID_URL)
+        headers={
+            "DNT": "1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+        }
+        h = requests.get(RANDOM_ID_URL, headers=headers)
+
         if h.status_code != 200:
-            return -1
+            print(h.status_code)
+            return None
         return h.text
 
     def __str__(self):
@@ -125,7 +137,7 @@ if __name__ == "__main__":
                       help="number of person to generate")
 
     parser.add_argument("--output-format", default="text", dest="output_format",
-                        choices=["text", "csv", "xml", "ad", "linux"],
+                        choices=["text", "csv", "xml", "windows", "linux"],
                         help="specify the output format")
 
     args = parser.parse_args()
@@ -147,7 +159,7 @@ if __name__ == "__main__":
         elif args.output_format == "csv":
             print(p.to_csv())
 
-        elif args.output_format == "ad":
+        elif args.output_format == "windows":
             print(p.as_windows_user())
 
         elif args.output_format == "linux":
