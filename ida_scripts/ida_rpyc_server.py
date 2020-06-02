c93c3279a568208663e0db59ed7933bee16d641b
hugsy <hugsy@blah.cat>
2020-06-02 11:41:21 -0700

    [ida_rpyc] somewhat py2 compat (but need py2+rpyc as client)

--- ida_scripts/ida_rpyc_server.py
+++ ida_scripts/ida_rpyc_server.py
@@ -1,5 +1,3 @@
-#!C:\Python38\python.exe
-
 """
 
 > "Because IDA sucks"
@@ -63,20 +61,20 @@ PLUGIN_VERSION = "0.2" # for IDA < 7.2 revert commits to 0.1
 PLUGIN_AUTHOR = "@_hugsy_"
 
 HOST, PORT = "0.0.0.0", 18812
-DEBUG = False
+DEBUG = True
 
-def xlog(x: str)  -> None:
-    sys.stderr.write(f"{threading.current_thread().name} - {x}\n") and sys.stderr.flush()
+def xlog(x):
+    sys.stderr.write("{} - {}\n".format(threading.current_thread().name, x)) and sys.stderr.flush()
 
-def err(msg: str) -> None:
-    xlog(f"[!] {msg}")
+def err(msg):
+    xlog("[!] {}".format(msg,))
 
-def ok(msg: str)  -> None:
-    xlog(f"[+] {msg}")
+def ok(msg):
+    xlog("[+] {}".format(msg,))
 
-def dbg(msg: str) -> None:
+def dbg(msg):
     if DEBUG:
-        xlog(f"[*] {msg}")
+        xlog("[*] {}".format(msg,))
 
 
 
@@ -84,11 +82,11 @@ class IdaWrapper:
     def __getattribute__(self, name):
         default = "IDoNotExistButNoReallyISeriouslyDoNotAndCannotExist"
 
-        dbg(f"trying to get {name}")
+        dbg("trying to get {}".format(name,))
 
         if name.startswith("exposed_"):
             name = name.replace("exposed_", "")
-            dbg(f"changed to get {name}")
+            dbg("changed to get {}".format(name,))
 
         val = getattr(idaapi, name, default)
         if val == default:
@@ -96,10 +94,10 @@ class IdaWrapper:
         if val == default:
             val = getattr(idautils, name, default)
         if val == default:
-            raise AttributeError(f"unknown {name}")
+            raise AttributeError("unknown {}".format(name,))
 
         if hasattr(val, '__call__'):
-            dbg(f"{val} is callable")
+            dbg("{} is callable".format(val,))
             def call(*args, **kwargs):
                 holder = [None] # need a holder, because 'global' sucks
 
@@ -111,7 +109,6 @@ class IdaWrapper:
                 return holder[0]
             return call
         else:
-            dbg(f"{val} is other")
             return val
 
 
@@ -124,17 +121,17 @@ class IdaRpycService(rpyc.Service):
 
 
     def on_connect(self, conn):
-        ok(f"connect open: {conn}")
+        ok("connect open: {}".format(conn,))
         return
 
 
     def on_disconnect(self, conn):
-        ok(f"connection closed: {conn}")
+        ok("connection closed: {}".format(conn,))
         return
 
 
     def exposed_iterate(self, iterator):
-        default = "IDoNotExistButNoReallyISeriouslyDoNotAndCannotExist %d" % (random.randint(0, 65535))
+        default = "IDoNotExistButNoReallyISeriouslyDoNotAndCannotExist {}".format(random.randint(0, 65535),)
         holder = [default]
         def trampoline():
             try:
@@ -177,4 +174,4 @@ if __name__ == "__main__":
     t = threading.Thread(target=start)
     t.daemon = True
     t.start()
-    ok(f"service listening on {HOST}:{PORT}...")
+    ok("service listening on {}:{}...".format(HOST, PORT))
