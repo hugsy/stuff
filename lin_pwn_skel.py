1a7c8889884730c0a16260dcb8fa721020a5252a
hugsy <hugsy@blah.cat>
2020-08-09 10:38:15 -0700

    lin_pwn_skel updates

--- lin_pwn_skel.py
+++ lin_pwn_skel.py
@@ -5,11 +5,11 @@ from pwn import *
 
 context.update(arch="amd64", # arch="i386", arch="mips", arch="arm",
                endian="little", os="linux", log_level="debug",
-               terminal=["tmux", "split-window", "-h", "-p 85"],)
+               terminal=["tmux", "split-window", "-h", "-p 65"],)
 
 
-LOCAL, REMOTE, SSH = False, False, False
-TARGET_ELF = os.path.realpath("./bof")
+LOCAL = True
+TARGET_ELF = os.path.realpath("./changeme")
 TARGET_LIBC = os.path.realpath("./libc.so")
 elf = ELF(TARGET_ELF)
 libc = ELF(TARGET_LIBC)
@@ -35,6 +35,7 @@ def attach(r):
             # "heap-analysis-helper",
             # "format-string-helper",
             # gdb_load_symbols_cmd(dbg_file, libc, r.libs()[libc.path]),
+            # "bp * $_base() + 0x1337",
         ]
         gdb.attach(r, '\n'.join(["break *{:#x}".format(x) for x in bkps] + cmds))
     return
@@ -42,18 +43,14 @@ def attach(r):
 
 def exploit(r):
     attach(r)
+    # r.sendlineafter(b"> ", b"HelloPwn" )
     r.interactive()
     return
 
 
 if __name__ == "__main__":
-    if len(sys.argv)==6 and sys.argv[1]=="ssh":
-        SSH = True
-        sh = ssh(host=sys.argv[2], port=int(sys.argv[3]), user=sys.argv[4], password=sys.argv[5])
-        sh.set_working_directory(os.path.dirname(TARGET_ELF))
-        r = sh.process([TARGET_ELF, ])
-    elif len(sys.argv)==3:
-        REMOTE = True
+    if len(sys.argv)>=2:
+        LOCAL = FALSE
         r = remote(sys.argv[1], int(sys.argv[2]))
     else:
         LOCAL = True
